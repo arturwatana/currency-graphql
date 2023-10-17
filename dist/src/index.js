803b4e6a11b12390ab70f7cb9f51cc648a7b4448
Artur Watanabe <arturwatanabe@gmail.com>
2023-10-17 04:17:01 -0300

    add: vercel deploy

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,27 +1,44 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import { main } from "./utils/db/mongoose.start.js";
 import { typeDefs } from "./schemas.gql.js";
 import { passwordHash } from "./utils/hash/index.js";
 import { getTokenAndSetUser } from "./utils/context/index.js";
 import { resolvers } from "./resolvers.gql.js";
 import { usersRepository } from "./modules/users/repository/index.js";
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+import http from "http";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 main().catch((err) => console.log(err));
 const services = {
     usersRepository,
     passwordHash
 };
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+await server.start();
+app.use('/', cors(), expressMiddleware(server, {
     context: async ({ req }) => {
         const user = getTokenAndSetUser(req.headers.authorization);
         return { user, BaseContext: services };
     }
-});
-console.log(`🚀  Server ready at: ${url}`);
+}));
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+//   context: async ({ req }) => {
+//     const user = getTokenAndSetUser(req.headers.authorization)
+//     return { user, BaseContext: services };
+//   }
+// });
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀  Server ready at: 4000`);
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,13 @@ import { resolvers } from "./resolvers.gql.js";
 import { usersRepository } from "./modules/users/repository/index.js";
 import { User } from "./modules/users/model/user.model.js";
 import { IUserRepository } from "./modules/users/repository/user.repository.js";
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { IHashPassword } from "./utils/hash/hash.interface.js";
+import { expressMiddleware } from '@apollo/server/express4';
+import http from "http";
+import express from "express";
+import cors from "cors";
+
 
 type ServicesProps = {
   userRepository: IUserRepository
@@ -22,9 +28,15 @@ export interface ContextProps {
   BaseContext: ServicesProps
 }
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer<BaseContext>({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
 main().catch((err) => console.log(err));
@@ -34,11 +46,24 @@ const services = {
   passwordHash
 }
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req }) => {
-    const user = getTokenAndSetUser(req.headers.authorization)
-    return { user, BaseContext: services };
-  }
-});
-console.log(`🚀  Server ready at: ${url}`);
+await server.start();
+app.use(
+  '/graphql',
+  cors<cors.CorsRequest>(),
+  expressMiddleware(server, {
+    context: async ({ req }) => {
+      const user = getTokenAndSetUser(req.headers.authorization)
+      return { user, BaseContext: services };
+    }
+  }),
+);
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+//   context: async ({ req }) => {
+//     const user = getTokenAndSetUser(req.headers.authorization)
+//     return { user, BaseContext: services };
+//   }
+// });
+
+await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀  Server ready at: 4000`);
