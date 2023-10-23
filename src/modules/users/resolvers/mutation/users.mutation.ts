8d1401b3d3d6381aa880fbbf884e2584d9580a04
Artur Watanabe <arturwatanabe@gmail.com>
2023-10-23 14:02:46 -0300

    add: username validation

--- src/modules/users/resolvers/mutation/users.mutation.ts
+++ src/modules/users/resolvers/mutation/users.mutation.ts
@@ -8,6 +8,10 @@ type CreateUserReq = {
 
 export const createUser = async (_, { data }: CreateUserReq, ctx: ContextProps) => {
   const user = User.create(data);
+  const userAlreadyExists = await ctx.BaseContext.usersRepository.getUserByUsername(user.username)
+  if(userAlreadyExists){
+    throw new Error("Usuário já existe") 
+  }
   user.password = await ctx.BaseContext.passwordHash.hash(data.password)
   await ctx.BaseContext.usersRepository.save(user);
   return user;
